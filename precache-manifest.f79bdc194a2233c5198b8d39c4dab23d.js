4d9da355651ce4beb98893fa41bb184c75046523
jaybenaim <benaimjacob@gmail.com>
2019-12-24 12:59:56 -0500

    Updates

---
+++ precache-manifest.f79bdc194a2233c5198b8d39c4dab23d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13ccbc008167788aeee3904d90da66bc",
+    "url": "/jaybenaim/card_games/index.html"
+  },
+  {
+    "revision": "30f2d0441712e249f8a6",
+    "url": "/jaybenaim/card_games/static/css/main.23b232eb.chunk.css"
+  },
+  {
+    "revision": "5938e270f95d3635b859",
+    "url": "/jaybenaim/card_games/static/js/2.8520b836.chunk.js"
+  },
+  {
+    "revision": "4aa0fe1eaee3dcf45338a38641d40f08",
+    "url": "/jaybenaim/card_games/static/js/2.8520b836.chunk.js.LICENSE"
+  },
+  {
+    "revision": "30f2d0441712e249f8a6",
+    "url": "/jaybenaim/card_games/static/js/main.92e8d9d6.chunk.js"
+  },
+  {
+    "revision": "2908aeb89f62561afeb3",
+    "url": "/jaybenaim/card_games/static/js/runtime-main.2e6a843e.js"
+  },
+  {
+    "revision": "d4876dceae84a7c124a039b8332a6566",
+    "url": "/jaybenaim/card_games/static/media/p1Back.d4876dce.jpg"
+  }
+]);
