274f4c233b2188fa99fcadaace83497c63a05214
jaybenaim <benaimjacob@gmail.com>
2019-12-25 22:08:37 -0500

    mobile and desktop poker table made for durak

--- card_games_frontend/src/components/Durak.js
+++ card_games_frontend/src/components/Durak.js
@@ -1,8 +1,19 @@
 import React, { Component } from "react";
+import "../assets/stylesheets/durak.css";
 class Durak extends Component {
   state = {};
   render() {
-    return <>Durak</>;
+    return (
+      <>
+        <div className="durak-container">
+          <div className="game-table">
+            <div className="outer-table">
+              <div className="center-table"> </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
   }
 }
 
